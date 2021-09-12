3f11629ebfb85b150f53ac8febb9dbed233c66e9
Przemek Tuzinek <przeo1996@gmail.com>
2021-09-13 00:15:11 +0200

    Added a SearchScreen

---
+++ lib/presentation/screens/search_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app_bloc/business_logic/blocs/weather_bloc/weather_bloc.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: TextField(
+          onChanged: (newString) {
+            BlocProvider.of<WeatherBloc>(context)
+                .add(CityWeatherRequested(cityName: newString));
+          },
+        ),
+      ),
+    );
+  }
+}
