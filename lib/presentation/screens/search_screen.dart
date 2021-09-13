d1171294f93c489ce73525bcfcdeb73a534f6070
Przemek Tuzinek <przeo1996@gmail.com>
2021-09-13 20:45:17 +0200

    SearchScreen UI update with Cancel button to go back.

--- lib/presentation/screens/search_screen.dart
+++ lib/presentation/screens/search_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:weather_app_bloc/business_logic/blocs/weather_bloc/weather_bloc.dart';
-import 'package:weather_app_bloc/data/constants/city_names.dart';
+import 'package:weather_app_bloc/data/data_providers/city_names_provider.dart';
 import 'package:weather_app_bloc/data/models/city.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -16,7 +16,6 @@ class _SearchScreenState extends State<SearchScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _typeAheadController = TextEditingController();
   final CityService cityService = CityService();
-  String _selectedCity = '';
 
   @override
   Widget build(BuildContext context) {
@@ -27,58 +26,77 @@ class _SearchScreenState extends State<SearchScreen> {
         child: Padding(
           padding: EdgeInsets.all(32.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text('What is your favorite city?'),
-              TypeAheadFormField<City>(
-                textFieldConfiguration: TextFieldConfiguration(
-                    controller: this._typeAheadController,
-                    decoration: InputDecoration(labelText: 'City')),
-                suggestionsCallback: (pattern) {
-                  return cityService.getSuggestions(pattern.trim());
-                },
-                itemBuilder: (context, suggestion) {
-                  return ListTile(
-                    //title: Text(suggestion),
-                    title: RichText(
-                      text: TextSpan(children: [
-                        TextSpan(
-                            text: suggestion.text1,
-                            style: TextStyle(color: Colors.grey)),
-                        TextSpan(
-                            text: suggestion.text2,
-                            style: TextStyle(color: Colors.black)),
-                        TextSpan(
-                            text: suggestion.text3,
-                            style: TextStyle(color: Colors.grey)),
-                      ]),
-                    ),
-                  );
-                },
-                transitionBuilder: (context, suggestionsBox, controller) {
-                  return suggestionsBox;
-                },
-                onSuggestionSelected: (City suggestion) {
-                  this._typeAheadController.text =
-                      suggestion.text1 + suggestion.text2 + suggestion.text3;
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please select a city';
-                  }
-                },
-                onSaved: (value) => this._selectedCity = value!,
-              ),
+              Text('Which city you want to check?'),
               SizedBox(
-                height: 10.0,
+                height: 20,
               ),
-              MaterialButton(
-                child: Text('Submit'),
-                onPressed: () {
-                  if (this._formKey.currentState!.validate()) {
-                    this._formKey.currentState!.save();
-                  }
-                },
-              )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible(
+                    child: TypeAheadFormField<City>(
+                      textFieldConfiguration: TextFieldConfiguration(
+                          controller: this._typeAheadController,
+                          decoration: InputDecoration(labelText: 'City')),
+                      suggestionsCallback: (pattern) {
+                        return cityService.getSuggestions(pattern.trim());
+                      },
+                      itemBuilder: (context, suggestion) {
+                        return ListTile(
+                          title: RichText(
+                            text: TextSpan(children: [
+                              TextSpan(
+                                  text: suggestion.text1,
+                                  style: TextStyle(color: Colors.grey)),
+                              TextSpan(
+                                  text: suggestion.text2,
+                                  style: TextStyle(color: Colors.black)),
+                              TextSpan(
+                                  text: suggestion.text3,
+                                  style: TextStyle(color: Colors.grey)),
+                            ]),
+                          ),
+                        );
+                      },
+                      transitionBuilder: (context, suggestionsBox, controller) {
+                        return suggestionsBox;
+                      },
+                      onSuggestionSelected: (City suggestion) {
+                        final String cityName = suggestion.text1 +
+                            suggestion.text2 +
+                            suggestion.text3;
+                        this._typeAheadController.text = cityName;
+                        BlocProvider.of<WeatherBloc>(context)
+                            .add(CityWeatherRequested(cityName: cityName));
+                        Navigator.pop(context);
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please select a city';
+                        }
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Flexible(
+                    child: MaterialButton(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 25),
+                        child: Text('Cancel'),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
