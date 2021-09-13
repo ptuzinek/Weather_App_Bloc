04bd04c64bd65849ad4b9752bd414e6044e11335
Przemek Tuzinek <przeo1996@gmail.com>
2021-09-13 20:44:09 +0200

    Made some Weather class properties dynamic to fix issue with int and double that alternate from the JSON. Also added method for calculating temperature metric.

--- lib/data/models/weather.dart
+++ lib/data/models/weather.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 
 class Weather {
   String cityName;
-  double temperature;
+  dynamic temperature;
   int conditionScore;
-  double windSpeed;
+  dynamic windSpeed;
   int cloudiness;
   int pressure;
   String weatherIconId;
@@ -57,4 +57,10 @@ class Weather {
   String toString() {
     return 'Weather(cityName: $cityName, temperature: $temperature, conditionScore: $conditionScore, windSpeed: $windSpeed, cloudiness: $cloudiness, pressure: $pressure, weatherIconId: $weatherIconId, weatherDescription: $weatherDescription)';
   }
+
+  String calculateCelsius(dynamic temperature) =>
+      num.parse((temperature - 273).toStringAsFixed(1)).toString();
+
+  String calculateFahrenheit(double temperature) =>
+      num.parse((1.8 * (temperature - 273) + 32).toStringAsFixed(1)).toString();
 }
